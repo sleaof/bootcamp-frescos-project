5def40b295af489a3598df79eaa0c8fe462ec94c
jesfernandes <jessikafernandes@mercadolivre.com>
2021-07-02 18:16:32 -0300

    Criacao das repositories

---
+++ src/main/java/com/mercadolibre/dambetan01/repository/BatchRepository.java
@@ -0,0 +1,6 @@
+package com.mercadolibre.dambetan01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BatchRepository extends JpaRepository<BatchRepository, Long> {
+}
