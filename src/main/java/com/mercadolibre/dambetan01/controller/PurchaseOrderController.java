b4b89581a05f93b06e7c752f2ea43b70bcceb219
juliabritob <julia.britobatista@mercadolivre.com>
2021-07-06 10:41:50 -0300

    criação da service de purchaseOrder + alguns ajustes

--- src/main/java/com/mercadolibre/dambetan01/controller/PurchaseOrderController.java
+++ src/main/java/com/mercadolibre/dambetan01/controller/PurchaseOrderController.java
@@ -1,16 +1,27 @@
 package com.mercadolibre.dambetan01.controller;
 
+import com.mercadolibre.dambetan01.dtos.response.ProductResponseDTO;
+import com.mercadolibre.dambetan01.model.Product;
 import com.mercadolibre.dambetan01.model.PurchaseOrder;
+import com.mercadolibre.dambetan01.repository.PurchaseOrderRepository;
+import com.mercadolibre.dambetan01.service.crud.IPurchaseOrderService;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping(path = "/api/v1")
 @RestController
 public class PurchaseOrderController {
 
-    @GetMapping("/fresh-products/")
-    public String selectedAllProducts(){
-        return "Deu bom";
+    private IPurchaseOrderService iPurchaseOrderService;
+
+    public PurchaseOrderController(IPurchaseOrderService iPurchaseOrderService) {
+        this.iPurchaseOrderService = iPurchaseOrderService;
     }
+
+    @GetMapping("/fresh-products/")
+    public String selectedAllProducts(){ return "Deu bom"; }
 
     @GetMapping("/fresh-products/list")
     public String selectedAllProductsByCategory(@RequestParam("category") String category){
@@ -18,8 +29,8 @@ public class PurchaseOrderController {
     }
 
     @GetMapping("/fresh-products/orders/")
-    public String selectedProductsFromOrder(@RequestParam("orderId") Long orderId){
-        return "Deu bom3";
+    public List<JSONObject> selectedProductsFromOrderId(@RequestParam("orderId") Long orderId){
+        return iPurchaseOrderService.selectProductsFromOrderId(orderId);
     }
 
     @PostMapping("/fresh-products/orders/")
