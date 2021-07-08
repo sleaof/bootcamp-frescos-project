4e29fcaf7be8b79be08d1540a979ba25dd5c3cbd
sleaof <stephanie.leao@mercadolivre.com>
2021-07-08 14:08:30 -0300

    Criação da repository e controller de Batch

--- src/main/java/com/mercadolibre/dambetan01/controller/BatchController.java
+++ src/main/java/com/mercadolibre/dambetan01/controller/BatchController.java
@@ -1,6 +1,7 @@
 package com.mercadolibre.dambetan01.controller;
 
 import com.mercadolibre.dambetan01.dtos.InboundOrderDTO;
+import com.mercadolibre.dambetan01.dtos.response.BatchResponseDTO;
 import com.mercadolibre.dambetan01.dtos.response.BatchStockResponseDTO;
 import com.mercadolibre.dambetan01.service.impl.BatServiceImpl;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RequestMapping(path = "/api/v1")
@@ -25,5 +27,15 @@ public class BatchController {
     @PutMapping("/fresh-products/inbounded/")
     public ResponseEntity<BatchStockResponseDTO> updateInboundOrder(@Valid @RequestBody InboundOrderDTO inboundOrderDTO, Long orderNumber) {
         return new ResponseEntity<>(batchService.updateBatchStock(inboundOrderDTO, orderNumber), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fresh-products/due-date/")
+    public ResponseEntity<List<BatchResponseDTO>> getAllBatchesOffAWarehouseByDueDate(@RequestParam("days") Integer days){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/fresh-products/due-date/list/")
+    public ResponseEntity<List<BatchResponseDTO>> getAllBatchesSortedByDueDateAndCategory(@RequestParam("days") Integer days, @RequestParam("category") String category){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/mercadolibre/dambetan01/repository/BatchRepository.java
+++ src/main/java/com/mercadolibre/dambetan01/repository/BatchRepository.java
@@ -1,7 +1,21 @@
 package com.mercadolibre.dambetan01.repository;
 
 import com.mercadolibre.dambetan01.model.Batch;
+import org.json.simple.JSONObject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BatchRepository extends JpaRepository<Batch, Long> {
+
+    @Query(value = "SELECT * FROM view_product_batch_in_warehouse " +
+            "WHERE due_date >= date(now()) + interval :days day", nativeQuery = true)
+    List<JSONObject> getAllBatchesOffAWarehouseByDueDate(@Param("days") Integer days);
+
+    @Query(value = "SELECT * FROM view_product_batch_in_warehouse " +
+            "WHERE due_date < date(now()) + interval 500 day AND product_category = 'FROZEN' " +
+            "ORDER BY due_date ASC", nativeQuery = true)
+    List<JSONObject> getAllBatchesSortedByDueDateAndCategory(@Param("days") Integer days, @Param("category") Integer category);
 }
