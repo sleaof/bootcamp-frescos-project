71b937bd429e8a718fe223f8c65aabe4fa1696bc
sleaof <stephanie.leao@mercadolivre.com>
2021-07-08 16:23:22 -0300

    Adicionar view ao requisito 05

--- src/main/java/com/mercadolibre/dambetan01/repository/BatchRepository.java
+++ src/main/java/com/mercadolibre/dambetan01/repository/BatchRepository.java
@@ -10,18 +10,12 @@ import java.util.List;
 
 public interface BatchRepository extends JpaRepository<Batch, Long> {
 
-    @Query(value = "SELECT B.batch_id AS batchNumber, B.product_id_fk AS productId, P.product_category AS productType, B.current_quantity AS quantity, B.due_date AS dueDate " +
-            "FROM batch AS B " +
-            "INNER JOIN products AS P ON B.product_id_fk = P.product_id " +
-            "INNER JOIN sections AS S ON B.section_id_fk = S.section_id " +
-            "WHERE due_date >= date(now()) + interval :days day", nativeQuery = true)
+    @Query(value = "select * from view_product_batch_in_warehouse " +
+            "WHERE dueDate >= date(now()) + interval :days day", nativeQuery = true)
     List<JSONObject> findAllBatchesOffAWarehouseByDueDate(@Param("days") Integer days);
 
-    @Query(value = "SELECT B.batch_id AS batchNumber, B.product_id_fk AS productId, P.product_category AS productType, B.current_quantity AS quantity, B.due_date AS dueDate " +
-            "FROM batch AS B " +
-            "INNER JOIN products AS P ON B.product_id_fk = P.product_id " +
-            "INNER JOIN sections AS S ON B.section_id_fk = S.section_id " +
-            "WHERE due_date >= date(now()) + interval :days day AND P.product_category = :category " +
-            "ORDER BY due_date ASC", nativeQuery = true)
+    @Query(value = "select * from view_product_batch_in_warehouse " +
+            "WHERE dueDate >= date(now()) + interval :days day AND productType= :category  " +
+            "ORDER BY dueDate ASC", nativeQuery = true)
     List<JSONObject> findAllBatchesSortedByDueDateAndCategory(@Param("days") Integer days, @Param("category") String category);
 }
